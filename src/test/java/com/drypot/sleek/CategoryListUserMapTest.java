e6bc27eb5a60ea76cc875d17ef91abd31b731d9c
drypot <drypot@gmail.com>
2020-07-13 13:12:01 +0900

    rename vars in CategoryListUserMap.

--- src/test/java/com/drypot/sleek/CategoryListUserMapTest.java
+++ src/test/java/com/drypot/sleek/CategoryListUserMapTest.java
@@ -13,37 +13,37 @@ class CategoryListUserMapTest {
     @Test
     @DisplayName("we can create categoryListUserMap")
     void create() {
-        CategoryListUserMap l = new CategoryListUserMap();
+        CategoryListUserMap map = new CategoryListUserMap();
     }
 
     @Test
     @DisplayName("we can put categoryList to userMap")
     void add() {
-        CategoryListUserMap l = new CategoryListUserMap();
+        CategoryListUserMap map = new CategoryListUserMap();
         CategoryList l1 = new CategoryList();
         CategoryList l2 = new CategoryList();
 
-        l.put("user1", l1);
-        l.put("user2", l2);
+        map.put("user1", l1);
+        map.put("user2", l2);
 
-        assertEquals(l1, l.get("user1"));
-        assertEquals(l2, l.get("user2"));
+        assertEquals(l1, map.get("user1"));
+        assertEquals(l2, map.get("user2"));
     }
 
     @Test
     @DisplayName("we can load categoryListUserMap from Json")
     void loadFromJson() throws IOException {
-        Config c = Config.fromCache();
-        CategoryListUserMap perUser = CategoryListUserMap.from(c.getJsonNode("categories"));
+        Config config = Config.fromCache();
+        CategoryListUserMap map = CategoryListUserMap.from(config.getJsonNode("categories"));
         CategoryList l;
 
-        l = perUser.get("user");
+        l = map.get("user");
         assertTrue(l.contains(100));
         assertTrue(l.contains(104));
         assertFalse(l.contains(60));
         assertFalse(l.contains(40));
 
-        l = perUser.get("cheater");
+        l = map.get("cheater");
         assertTrue(l.contains(100));
         assertTrue(l.contains(104));
         assertTrue(l.contains(60));
