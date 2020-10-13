45b808ff06b6ce3a4289a6f45267437063ab9b7e
drypot <drypot@gmail.com>
2020-10-13 11:18:32 +0900

    update ApplicationTests.

--- src/test/java/com/drypot/sleek/ApplicationTests.java
+++ src/test/java/com/drypot/sleek/ApplicationTests.java
@@ -1,35 +1,31 @@
 package com.drypot.sleek;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
     @Autowired
     Environment environment;
 
     @Test
-    @DisplayName("True should be true")
     void trueShouldBeTrue() {
-        assertTrue(true);
+        assertThat(true).isTrue();
     }
 
     @Test
-    @DisplayName("Profile should be test")
     void profileShouldBeTest() {
         assertThat(environment.getActiveProfiles()[0]).isEqualTo("test");
     }
 
     @Test
-    @DisplayName("App name should be test")
     void appNameShouldBeTest() {
         assertThat(environment.getProperty("app.name")).isEqualTo("sleek test");
     }
