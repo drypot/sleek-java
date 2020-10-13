d4d019b18a00eae9bf17244705f44877dfd0c4c0
drypot <drypot@gmail.com>
2020-10-13 13:49:50 +0900

    update CategoryListMapJsonLoader.

--- src/test/java/com/drypot/sleek/CategoryListMapJsonLoaderTest.java
+++ src/test/java/com/drypot/sleek/CategoryListMapJsonLoaderTest.java
@@ -1,12 +1,9 @@
 package com.drypot.sleek;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
--- src/test/java/com/drypot/sleek/CategoryListTest.java
+++ src/test/java/com/drypot/sleek/CategoryListTest.java
@@ -1,6 +1,5 @@
 package com.drypot.sleek;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
