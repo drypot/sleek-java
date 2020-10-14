e6a06ffae896bdc1d6f9fead4aaff6ad65aa4623
drypot <drypot@gmail.com>
2020-10-14 11:32:46 +0900

    remove field injection from Application.

--- src/main/java/com/drypot/sleek/Application.java
+++ src/main/java/com/drypot/sleek/Application.java
@@ -1,25 +1,18 @@
 package com.drypot.sleek;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-    @Autowired
-    Environment environment;
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("hello, world!");
-        System.out.println("app.name = " + environment.getProperty("app.name"));
-        System.out.println("app.port = " + environment.getProperty("app.port", Integer.class));
+        SpringApplication app = new SpringApplication(Application.class);
+        ApplicationContext context = app.run(args);
+        Environment env = context.getEnvironment();
+        System.out.println("app.name = " + env.getProperty("app.name"));
+        System.out.println("app.port = " + env.getProperty("app.port", Integer.class));
     }
 }
