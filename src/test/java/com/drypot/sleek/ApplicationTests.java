966cbe726253457efe016face44dd9577ad18971
drypot <drypot@gmail.com>
2020-10-15 10:56:39 +0900

    update ApplicationTests.

--- src/test/java/com/drypot/sleek/ApplicationTests.java
+++ src/test/java/com/drypot/sleek/ApplicationTests.java
@@ -3,14 +3,18 @@ package com.drypot.sleek;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(classes = {})
 @ActiveProfiles("test")
 class ApplicationTests {
+
+    @Autowired
+    ApplicationContext context;
 
     @Autowired
     Environment env;
@@ -18,6 +22,16 @@ class ApplicationTests {
     @Test
     void trueShouldBeTrue() {
         assertThat(true).isTrue();
+    }
+
+    @Test
+    void contextShouldBeExists() {
+        assertThat(context).isNotNull();
+    }
+
+    @Test
+    void envShouldBeExists() {
+        assertThat(env).isNotNull();
     }
 
     @Test
