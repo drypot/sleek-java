7beec481db6881745372483c36e9d9bda2565850
drypot <drypot@gmail.com>
2020-10-17 19:09:58 +0900

    add HelloController.

---
+++ src/test/java/com/drypot/sleek/controller/HelloControllerTest.java
@@ -0,0 +1,15 @@
+package com.drypot.sleek.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@WebMvcTest
+@ActiveProfiles("test")
+class HelloControllerTest {
+
+    @Test
+    void hello() {
+
+    }
+}
